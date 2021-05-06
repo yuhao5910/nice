d35bd84e8f15981dc87c273235e7918e66f30bd8
yuhao5910 <990803344@qq.com>
2021-05-06 17:47:41 +0800

    main

--- mybutton.py
+++ mybutton.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import *
 class myplaybtn(QPushButton):
+    """开始类"""
     def __init__(self,parent=None):
         super(myplaybtn, self).__init__(parent)
         self.setText('play')
         self.move(250,200)
 class myendbtn(QPushButton):
+    """结束类"""
     def __init__(self,parent=None):
         super(myendbtn, self).__init__(parent)
         self.setText('end')
--- mywidget.py
+++ mywidget.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 class mywidget(QWidget):
+    """窗口类"""
     def __init__(self):
         super(mywidget, self).__init__()
         self.setWindowTitle("nice")
