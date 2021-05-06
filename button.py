1688659822708c5379ee322ba5dc8a13005507b9
yuhao5910 <990803344@qq.com>
2021-05-06 17:06:35 +0800

    main

---
+++ button.py
@@ -0,0 +1,5 @@
+from PyQt5.QtWidgets import *
+class button(QPushButton):
+    def __init__(self,text='',parent=None):
+        QWidget.__init__()
+        QWidget
--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+import sys
+import widget
 print("Hello World!")
 print("Nice to meet you!")
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    widget = QWidget()
+    widget.setWindowTitle("nice")
+    widget.setWindowIcon(QIcon("img/girl_0.jpg"))
+    widget.setFixedSize(600, 500)
+    playbtn=QPushButton("Play",widget)
+    endbtn=QPushButton("end",widget)
+    playbtn.move(250,200)
+    endbtn.move(250,250)
+    widget.show()
+    sys.exit(app.exec_())
+
+
+
