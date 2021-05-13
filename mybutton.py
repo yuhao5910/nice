dd562501d8ba2038e38cb6bcbc4b63ec7265f0d1
yuhao5910 <990803344@qq.com>
2021-05-13 17:56:56 +0800

    main

--- mybutton.py
+++ mybutton.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt5.QtWidgets import *
 class myplaybtn(QPushButton):
     """开始类"""
@@ -5,9 +7,11 @@ class myplaybtn(QPushButton):
         super(myplaybtn, self).__init__(parent)
         self.setText('play')
         self.move(250,200)
+        self.clicked.connect(lambda :print("play"))
 class myendbtn(QPushButton):
     """结束类"""
     def __init__(self,parent=None):
         super(myendbtn, self).__init__(parent)
         self.setText('end')
         self.move(250,250)
+        self.clicked.connect(lambda : sys.exit())
--- mymenubar.py
+++ mymenubar.py
@@ -1,11 +1,19 @@
 from PyQt5.QtWidgets import *
 class mymenubar(QMenuBar):
+    """菜单类"""
     def __init__(self,parent=None):
         super(mymenubar, self).__init__(parent)
         myfile = self.addMenu("文件(F)")
         myedit = self.addMenu("编辑(E)")
-        myfile.addAction("新建(N)")
-        myfile.addAction("打开(O)")
-        myfile.addAction("保存(S)")
+        mynew=myfile.addAction("新建(N)")
+        myopen=myfile.addAction("打开(O)")
+        mysave=myfile.addAction("保存(S)")
+        myedit.addAction("剪切(T)")
+        myedit.addAction("复制(C)")
+
+        """功能实现"""
+        mynew.triggered.connect(lambda :print("新建"))
+        myopen.triggered.connect(lambda: print("打开"))
+        mysave.triggered.connect(lambda: print("保存"))
 
 
